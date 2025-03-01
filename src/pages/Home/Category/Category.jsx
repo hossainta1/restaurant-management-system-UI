28de3b0232456425455b3d4eaef192811af08645
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-01 21:08:26 +0600

    Impliment Section title

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -10,41 +10,57 @@ import slider2 from "../../../assets/home/slide2.jpg";
 import slider3 from "../../../assets/home/slide3.jpg";
 import slider4 from "../../../assets/home/slide4.jpg";
 import slider5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper mb-24"
-    >
-      <SwiperSlide>
-        <img src={slider1} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">salad</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slider2} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">Pizza</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slider3} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">soup</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slider4} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">Desserts</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slider5} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">salad</h3>
-       
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <SectionTitle 
+      subHeading={"From 11:00 am to 10:00pm"}
+      heading={"Order Online"}
+      ></SectionTitle>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img src={slider1} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            salad
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider2} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider3} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            soup
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider4} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider5} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            salad
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
