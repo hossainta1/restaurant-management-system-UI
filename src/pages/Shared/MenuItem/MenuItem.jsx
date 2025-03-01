e4d537c937890d70ba47f384cc9b80169182fee6
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-01 22:04:36 +0600

    Load menu data

---
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MenuItem = ({item}) => {
+    const {name, image, recipe, price} = item;
+    return (
+        <div className='flex space-x-4'>
+            <img className='w-[120px] rounded-r-lg' src={image} alt="" />
+            <div>
+                <h3 className='uppercase'>{name}</h3>
+                <p>{recipe}-------</p>
+            </div>
+            <p className='text-yellow-500'>${price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;
