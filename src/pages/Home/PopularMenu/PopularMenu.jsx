12ca9bdc66b000c33213a1cca47fd576d3b2ef07
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-02 19:49:05 +0600

    Change button

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from '../../../components/SectionTitle/SectionTitle';
-import MenuItem from '../../Shared/MenuItem/MenuItem';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
+  const [menu, setMenu] = useState([]);
 
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === "popular");
-            setMenu(popularItems)})
-    }, [])
-    return (
-        <section className='mb-12'>
-            <SectionTitle
-            heading="Check our Menu"
-            subHeading= "Popular Items"
-            ></SectionTitle>
-            <div className='grid md:grid-cols-2 gap-4'>
-                {
-                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-                }
-            </div>
-            
-        </section>
-    );
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="Check our Menu"
+        subHeading="Popular Items"
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-4">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+
+      <div className="flex justify-center items-center">
+        <button className="btn btn-outline btn-primary border-0 border-b-4 mt-4">
+          View full Menu
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
